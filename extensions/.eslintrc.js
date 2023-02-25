Skyhigh173/extensions

Forbid using APIs replaced by Scratch.*

.eslintrc.js
@@ -76,6 +76,71 @@ module.exports = {
     // significantly impacts the behavior of projects.
     'require-await': 'error',
     // Require each extension to use strict mode
-    'strict': ['error', 'function']
+    'strict': ['error', 'function'],
+    // Disallow APIs that are replaced by Scratch.* APIs
+    'no-restricted-syntax': [
+      'error',
+      // This is not comprehensive, but it should be enough to prevent the most common ways for these to be written.
+      {
+        selector: 'CallExpression[callee.name=fetch]',
+        message: 'Use Scratch.fetch() instead of fetch()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=window][callee.property.name=fetch]',
+        message: 'Use Scratch.fetch() instead of window.fetch()'
+      },
+      {
+        selector: 'CallExpression[callee.name=open]',
+        message: 'Use Scratch.openWindow() instead of open()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=window][callee.property.name=open]',
+        message: 'Use Scratch.openWindow() instead of window.open()'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.name=location][left.property.name=href]',
+        message: 'Use Scratch.redirect() instead of location.href = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.object.name=window][left.object.property.name=location][left.property.name=href]',
+        message: 'Use Scratch.redirect() instead of window.location.href = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.name=location]',
+        message: 'Use Scratch.redirect() instead of location = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.name=window][left.property.name=location]',
+        message: 'Use Scratch.redirect() instead of window.location = ...'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=location][callee.property.name=assign]',
+        message: 'Use Scratch.redirect() instead of location.assign()'
+      },
+      {
+        selector: 'CallExpression[callee.object.object.name=window][callee.object.property.name=location][callee.property.name=assign]',
+        message: 'Use Scratch.redirect() instead of window.location.assign()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=location][callee.property.name=replace]',
+        message: 'Use Scratch.redirect() instead of location.replace()'
+      },
+      {
+        selector: 'CallExpression[callee.object.object.name=window][callee.object.property.name=location][callee.property.name=replace]',
+        message: 'Use Scratch.redirect() instead of window.location.replace()'
+      },
+      {
+        selector: 'NewExpression[callee.name=XMLHttpRequest]',
+        message: 'Use Scratch.fetch() instead of XMLHttpRequest'
+      },
+      {
+        selector: 'NewExpression[callee.name=WebSocket]',
+        message: 'Ensure that `await Scratch.canFetch(url)` is checked first, then add // eslint-ignore-next-line no-restricted-syntax'
+      },
+      {
+        selector: 'NewExpression[callee.name=Image]',
+        message: 'Ensure that `await Scratch.canFetch(url)` is checked first, then add // eslint-ignore-next-line no-restricted-syntax'
+      }
+    ]
   }
 };

OmniBlocks/monorepo

extensions/.eslintrc.js
@@ -76,6 +76,75 @@ module.exports = {
     // significantly impacts the behavior of projects.
     'require-await': 'error',
     // Require each extension to use strict mode
-    'strict': ['error', 'function']
+    'strict': ['error', 'function'],
+    // Disallow APIs that are replaced by Scratch.* APIs
+    'no-restricted-syntax': [
+      'error',
+      // This is not comprehensive, but it should be enough to prevent the most common ways for these to be written.
+      {
+        selector: 'CallExpression[callee.name=fetch]',
+        message: 'Use Scratch.fetch() instead of fetch()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=window][callee.property.name=fetch]',
+        message: 'Use Scratch.fetch() instead of window.fetch()'
+      },
+      {
+        selector: 'CallExpression[callee.name=open]',
+        message: 'Use Scratch.openWindow() instead of open()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=window][callee.property.name=open]',
+        message: 'Use Scratch.openWindow() instead of window.open()'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.name=location][left.property.name=href]',
+        message: 'Use Scratch.redirect() instead of location.href = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.object.name=window][left.object.property.name=location][left.property.name=href]',
+        message: 'Use Scratch.redirect() instead of window.location.href = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.name=location]',
+        message: 'Use Scratch.redirect() instead of location = ...'
+      },
+      {
+        selector: 'AssignmentExpression[left.object.name=window][left.property.name=location]',
+        message: 'Use Scratch.redirect() instead of window.location = ...'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=location][callee.property.name=assign]',
+        message: 'Use Scratch.redirect() instead of location.assign()'
+      },
+      {
+        selector: 'CallExpression[callee.object.object.name=window][callee.object.property.name=location][callee.property.name=assign]',
+        message: 'Use Scratch.redirect() instead of window.location.assign()'
+      },
+      {
+        selector: 'CallExpression[callee.object.name=location][callee.property.name=replace]',
+        message: 'Use Scratch.redirect() instead of location.replace()'
+      },
+      {
+        selector: 'CallExpression[callee.object.object.name=window][callee.object.property.name=location][callee.property.name=replace]',
+        message: 'Use Scratch.redirect() instead of window.location.replace()'
+      },
+      {
+        selector: 'NewExpression[callee.name=XMLHttpRequest]',
+        message: 'Use Scratch.fetch() instead of XMLHttpRequest'
+      },
+      {
+        selector: 'NewExpression[callee.name=WebSocket]',
+        message: 'Ensure that `await Scratch.canFetch(url)` is checked first, then add // eslint-disable-next-line no-restricted-syntax'
+      },
+      {
+        selector: 'NewExpression[callee.name=Image]',
+        message: 'Ensure that `await Scratch.canFetch(url)` is checked first, then add // eslint-disable-next-line no-restricted-syntax'
+      },
+      {
+        selector: 'NewExpression[callee.name=Audio]',
+        message: 'Ensure that `await Scratch.canFetch(url)` is checked first, then add // eslint-disable-next-line no-restricted-syntax'
+      }
+    ]
   }
 };
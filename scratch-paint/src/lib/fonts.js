0c5378934108fdea44274ed4d368395638e2d99b
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-03-22 16:25:59 -0600

    Revert "Merge pull request #4 from PuzzlingGGG/develop"
    This reverts commit 0f5710bfa64ba84e20196e7adfaad3f2e8589519, reversing
    changes made to 244502de120a042266c431ef6586ee949b2f65a3.

--- scratch-paint/src/lib/fonts.js
+++ scratch-paint/src/lib/fonts.js
@@ -12,7 +12,7 @@ const Fonts = {
     ARCADE: 'Arcade',
     ARCHIVO: 'Archivo',
     ARCHIVOBLACK: 'Archivo Black',
-    SCRATCH: 'Branches',
+    SCRATCH: 'Scratch',
     CHINESE: '"Microsoft YaHei", "微软雅黑", STXihei, "华文细黑"',
     JAPANESE: '"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic"',
     KOREAN: 'Malgun Gothic'

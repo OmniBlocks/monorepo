c5b6628bda672dc4616d311243742b06864fcd4b
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-08 23:25:58 -0700

    revert stylings here

--- src/components/mode-tools/mode-tools.jsx
+++ src/components/mode-tools/mode-tools.jsx
@@ -215,7 +215,7 @@ const ModeToolsComponent = props => {
                         />
                         
                         {hasSimplifyOption && (
-                            <Label text={props.intl.formatMessage(messages.brushSimplify)} style={{ marginLeft: "calc(2 * .25rem)" }}>
+                            <Label text={props.intl.formatMessage(messages.brushSimplify)}>
                                 <LiveInput
                                     range
                                     small
@@ -262,7 +262,7 @@ const ModeToolsComponent = props => {
                         />
 
                         {hasSimplifyOption && (
-                            <Label text={props.intl.formatMessage(messages.eraserSimplify)} style={{ marginLeft: "calc(2 * .25rem)" }}>
+                            <Label text={props.intl.formatMessage(messages.eraserSimplify)}>
                                 <LiveInput
                                     range
                                     small
@@ -419,7 +419,7 @@ const ModeToolsComponent = props => {
                 const changeFunctionSimplify = props.onPenSimplifySliderChange;
                 return (
                     <div className={classNames(props.className, styles.modeTools)}>
-                        <Label text={props.intl.formatMessage(messages.eraserSimplify)} style={{ marginLeft: "calc(2 * .25rem)" }}>
+                        <Label text={props.intl.formatMessage(messages.eraserSimplify)}>
                             <LiveInput
                                 range
                                 small

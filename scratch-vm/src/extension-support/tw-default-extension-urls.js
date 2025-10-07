5f5be0cadcdfba10fc1adb2f37622b267080deec
Thomas Weber <muffin@muffin.ink>
2025-10-07 18:06:10 -0500

    Add face sensing to default extension URLs

--- scratch-vm/src/extension-support/tw-default-extension-urls.js
+++ scratch-vm/src/extension-support/tw-default-extension-urls.js
@@ -9,7 +9,10 @@ const defaults = {
     text: 'https://extensions.turbowarp.org/lab/text.js',
 
     // Turboloader's AudioStream
-    audiostr: 'https://extensions.turbowarp.org/turboloader/audiostream.js'
+    audiostr: 'https://extensions.turbowarp.org/turboloader/audiostream.js',
+
+    // https://scratch.mit.edu/discuss/topic/842592/
+    faceSensing: 'https://extensions.turbowarp.org/lab/face-sensing.js'
 };
 
 module.exports = defaults;

21d310d0891bf1a8e339a5ce139b2cae79e81ab8
supervoidcoder <supervoidcoder@users.noreply.github.com>
2026-06-09 21:13:45 -0400

    feat: omg im making progeress guys

--- update.sh
+++ update.sh
@@ -5,50 +5,55 @@ git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 
 pip install git-filter-repo
 
-mkdir scratch-gui-temp
-cd scratch-gui-temp
-git init 
-git remote add upstream https://github.com/TurboWarp/scratch-gui.git
-git fetch upstream develop
-git checkout -b develop placeholder
+cd ../ 
+mkdir temp-monorepo
+cd temp-monorepo
+git init
+
+git remote add upstream-gui https://github.com/TurboWarp/scratch-gui.git
+git fetch upstream-gui develop
+git checkout -b scratch-gui upstream-gui/develop
 git-filter-repo --to-subdirectory-filter scratch-gui 
 
-cd ../
 
-mkdir scratch-blocks-temp
-cd scratch-blocks-temp
-git init
-git remote add upstream https://github.com/TurboWarp/scratch-blocks.git
-git fetch upstream develop
-git checkout -b develop placeholder
+
+git remote add upstream-blocks https://github.com/TurboWarp/scratch-blocks.git
+git fetch upstream-blocks develop
+git checkout -b scratch-blocks upstream-blocks/develop
 git-filter-repo --to-subdirectory-filter scratch-blocks
 
-cd ../
 
-mkdir scratch-vm-temp
-cd scratch-vm-temp
-git init
-git remote add upstream https://github.com/TurboWarp/scratch-vm.git
-git fetch upstream develop
-git checkout -b develop placeholder
+
+
+git remote add upstream-vm https://github.com/TurboWarp/scratch-vm.git
+git fetch upstream-vm develop
+git checkout -b scratch-vm upstream-vm/develop
 git-filter-repo --to-subdirectory-filter scratch-vm
 
-cd ../
 
-mkdir scratch-render
-cd scratch-render
-git init
-git remote add upstream https://github.com/TurboWarp/scratch-render.git
-git fetch upstream develop
-git checkout -b develop placeholder
+
+
+git remote add upstream-render https://github.com/TurboWarp/scratch-render.git
+git fetch upstream-render develop
+git checkout -b scratch-render upstream-render/develop
 git-filter-repo --to-subdirectory-filter scratch-render
 
-git checkout -b upstream-update
+git merge scratch-gui --allow-unrelated-histories --no-edit
+git merge scratch-blocks --allow-unrelated-histories --no-edit
+git merge scratch-vm --allow-unrelated-histories --no-edit
+git merge scratch-render --allow-unrelated-histories --no-edit
  
-cd ..
+
+
+
+cd ../monorepo
+
+git remote add upstream ../temp-monorepo
+
+git checkout -b upstream-update
 
 # TODO: parse it to look good in the PR description 
 
 
 
-if git merge  ./scratch-gui-temp --allow-unrelated-histories --no-edit
+if git merge upstream/develop --allow-unrelated-histories --no-edit

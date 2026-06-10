25f40b9f88f0497b5a45ce9aecd783d265e0bdda
supervoidcoder <supervoidcoder@users.noreply.github.com>
2026-06-09 21:38:11 -0400

    fix: sometimes you need to force git to do stuff whether it wants to or not

--- update.sh
+++ update.sh
@@ -64,6 +64,6 @@ if git merge upstream/develop --allow-unrelated-histories --no-edit; then
     gh pr create --head upstream-update --base main --title "Upstream update $(date)" --body "Updated packages from upstream. pls review" --reviewer "OmniBlocks/coders" 
 else
     git add . 
-    git commit -m "chore: UPDATE UPSTREAM $(date)"
-    git push origin upstream-update
+    git commit --no-edit
+    git push origin upstream-update --force
     gh pr create --head upstream-update --base main --title "Upstream update $(date) (has conflicts)" --body "# THERE ARE CONFLICTS IN THIS AUTOMATIC PR. PLEASE DO NOT MERGE UNTIL THEY ARE RESOLVED. ⚠️🚨⚠️🚨⚠️🚨⚠️🚨⚠️🚨⚠️🚨" --draft --reviewer "OmniBlocks/coders"

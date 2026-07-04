68d6a480f6810534df4e8be56ef918adc2e6498e
Thomas Weber <muffin@muffin.ink>
2026-07-04 17:56:47 -0500

    Submit more specific windchime event on error

--- scratch-gui/src/containers/tw-windchime-submitter.jsx
+++ scratch-gui/src/containers/tw-windchime-submitter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {getIsError} from '../reducers/project-state';
+import {ProjectUnsharedError, ProjectFetchError} from '../lib/tw-load-project-error';
 
 const ENDPOINT = 'https://windchimes.turbowarp.org/api/chime';
 const OPT_OUT_KEY = 'tw:windchime_opt_out';
@@ -26,6 +27,16 @@ const isOptedOut = () => {
     // but if someone has these set, it's good to assume that they would opt out if given the choice.
     // So we'll just respect that preemptively.
     return navigator.globalPrivacyControl || navigator.doNotTrack === '1';
+};
+
+const getErrorEvent = error => {
+    if (error instanceof ProjectUnsharedError) {
+        return 'error/unshared';
+    }
+    if (error instanceof ProjectFetchError) {
+        return 'error/fetch';
+    }
+    return 'error/loading';
 };
 
 const submitChime = async (resource, event) => {
@@ -70,7 +81,7 @@ class TWWindchimeSubmitter extends React.Component {
             !submittedErrorsThisSession.has(this.props.projectId)
         ) {
             submittedErrorsThisSession.add(this.props.projectId);
-            submitChime(`scratch/${this.props.projectId}`, 'error/loading');
+            submitChime(`scratch/${this.props.projectId}`, getErrorEvent(this.props.error));
         }
     }
 
@@ -81,6 +92,7 @@ class TWWindchimeSubmitter extends React.Component {
 }
 
 TWWindchimeSubmitter.propTypes = {
+    error: PropTypes.any,
     isEmbedded: PropTypes.bool.isRequired,
     isError: PropTypes.bool.isRequired,
     isStarted: PropTypes.bool.isRequired,
@@ -88,6 +100,7 @@ TWWindchimeSubmitter.propTypes = {
 };
 
 const mapStateToProps = state => ({
+    error: state.scratchGui.projectState.error,
     isEmbedded: state.scratchGui.mode.isEmbedded,
     isStarted: state.scratchGui.vmStatus.running,
     isError: getIsError(state.scratchGui.projectState.loadingState),

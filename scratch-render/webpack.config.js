0272fda3c95b3e3029c6f5ce2c119b5757501683
Oliver <oliverjohnkim@icloud.com>
2026-07-11 19:17:55 -0500

    webpack 5

--- scratch-render/webpack.config.js
+++ scratch-render/webpack.config.js
@@ -1,94 +1,100 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 const base = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
     devServer: {
-        contentBase: false,
-        host: '0.0.0.0',
-        port: process.env.PORT || 8361
+        static: false,
+        host: "0.0.0.0",
+        port: process.env.PORT || 8361,
     },
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
     module: {
         rules: [
             {
-                include: [
-                    path.resolve('src')
-                ],
+                include: [path.resolve("src")],
                 test: /\.js$/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 options: {
-                    presets: [['env', {targets: {browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']}}]]
-                }
-            }
-        ]
+                    presets: [
+                        [
+                            "env",
+                            {
+                                targets: {
+                                    browsers: [
+                                        "last 3 versions",
+                                        "Safari >= 8",
+                                        "iOS >= 8",
+                                    ],
+                                },
+                            },
+                        ],
+                    ],
+                },
+            },
+        ],
     },
     optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                include: /\.min\.js$/
-            })
-        ]
+        minimizer: [],
     },
-    plugins: []
+    plugins: [],
 };
 
 module.exports = [
     // Playground
     Object.assign({}, base, {
-        target: 'web',
+        target: "web",
         entry: {
-            playground: './src/playground/playground.js',
-            queryPlayground: './src/playground/queryPlayground.js'
+            playground: "./src/playground/playground.js",
+            queryPlayground: "./src/playground/queryPlayground.js",
         },
         output: {
-            libraryTarget: 'umd',
-            path: path.resolve('playground'),
-            filename: '[name].js'
+            libraryTarget: "umd",
+            path: path.resolve("playground"),
+            filename: "[name].js",
         },
         plugins: base.plugins.concat([
             new CopyWebpackPlugin([
                 {
-                    context: 'src/playground',
-                    from: '*.+(html|css)'
-                }
-            ])
-        ])
+                    context: "src/playground",
+                    from: "*.+(html|css)",
+                },
+            ]),
+        ]),
     }),
     // Web-compatible
     Object.assign({}, base, {
-        target: 'web',
+        target: "web",
         entry: {
-            'scratch-render': './src/index.js',
-            'scratch-render.min': './src/index.js'
+            "scratch-render": "./src/index.js",
+            "scratch-render.min": "./src/index.js",
         },
         output: {
-            library: 'ScratchRender',
-            libraryTarget: 'umd',
-            path: path.resolve('dist', 'web'),
-            filename: '[name].js'
-        }
+            library: "ScratchRender",
+            libraryTarget: "umd",
+            path: path.resolve("dist", "web"),
+            filename: "[name].js",
+        },
     }),
     // Node-compatible
     Object.assign({}, base, {
-        target: 'node',
+        target: "node",
         entry: {
-            'scratch-render': './src/index.js'
+            "scratch-render": "./src/index.js",
         },
         output: {
-            library: 'ScratchRender',
-            libraryTarget: 'commonjs2',
-            path: path.resolve('dist', 'node'),
-            filename: '[name].js'
+            library: "ScratchRender",
+            libraryTarget: "commonjs2",
+            path: path.resolve("dist", "node"),
+            filename: "[name].js",
         },
         externals: {
-            '!ify-loader!grapheme-breaker': 'grapheme-breaker',
-            '!ify-loader!linebreak': 'linebreak',
-            'hull.js': true,
-            '@turbowarp/scratch-svg-renderer': true,
-            'twgl.js': true,
-            'xml-escape': true
-        }
-    })
+            "!ify-loader!grapheme-breaker": "grapheme-breaker",
+            "!ify-loader!linebreak": "linebreak",
+            "hull.js": true,
+            "@turbowarp/scratch-svg-renderer": true,
+            "twgl.js": true,
+            "xml-escape": true,
+        },
+    }),
 ];

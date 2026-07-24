f3f9ad473bc7a959ed470e7db82995b8cd377d28
Oliver <oliverjohnkim@icloud.com>
2026-07-23 20:24:36 -0500

    first steps

---
+++ scratch-gui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.omniblocks.app',
+  appName: 'OmniBlocks',
+  webDir: 'build'
+};
+
+export default config;

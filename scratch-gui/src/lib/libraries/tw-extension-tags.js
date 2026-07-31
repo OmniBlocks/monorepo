fceb1f0bf258f6bd5a957501a18de53ddbb08974
AmpElectrecuted <266199358+ampelc@users.noreply.github.com>
2026-07-31 19:39:14 +0100

    Delete scratch-gui directory

--- scratch-gui/src/lib/libraries/tw-extension-tags.js
+++
@@ -1,8 +0,0 @@
-import {APP_NAME} from '../brand';
-
-// Because these are all brand names, it is unnecessary for them to be translatable.
-export default [
-    {tag: 'scratch', intlLabel: 'Scratch'},
-    {tag: 'tw', intlLabel: "TurboWarp"},
-    {tag: 'ob', intlLabel: "OmniBlocks"}
-];
